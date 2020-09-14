0052809073d5883fab1c2998f61a0f0223a2011a
Jody Eggleston <jmeggles@gmail.com>
2020-09-13 22:47:54 -0400

    added navigation components

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,14 +1,25 @@
 import React, { Component, Fragment } from "react";
+import { Nav, Navbar } from "react-bootstrap"; // import Nav, Navbar if used below
 
 class Navigation extends Component {
-    render () {
-        return (
-            <Fragment>
-                <p>Navigation Component</p>
-            </Fragment>
-        )
-    }
-
+  render() {
+    return (
+      <Fragment>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Jody Eggleston</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">HOME</Nav.Link>
+              <Nav.Link href="#about">ABOUT</Nav.Link>
+              <Nav.Link href="#projects">PROJECTS</Nav.Link>
+              <Nav.Link href="#contact">CONTACT</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Fragment>
+    );
+  }
 }
 
 export default Navigation;
