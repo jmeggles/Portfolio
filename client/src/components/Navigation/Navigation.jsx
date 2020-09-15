1d4e9f1ebe446f37d0038d90ba56968dbe68253a
Jody Eggleston <jmeggles@gmail.com>
2020-09-15 13:37:17 -0400

    adding navigation

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,12 +1,13 @@
 import React, { Component, Fragment } from "react";
 import { Nav, Navbar } from "react-bootstrap"; // import Nav, Navbar if used below
+import "./style.css";
 
 class Navigation extends Component {
   render() {
     return (
       <Fragment>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">Jody Eggleston</Navbar.Brand>
+        <Navbar bg="light" expand="sm">
+          <Navbar.Brand id="homePageName" href="#home">Jody Eggleston</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
