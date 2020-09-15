9fbe2230ff9a27261b24c9ef837eb3134517ba01
Jody Eggleston <jmeggles@gmail.com>
2020-09-15 14:13:00 -0400

    added project cards

---
+++ client/src/components/Projects/Projects.jsx
@@ -0,0 +1,72 @@
+import React, { Component, Fragment } from "react";
+import { Card, Button, Col, Container, Row } from "react-bootstrap"; //import all contexts
+import Placeholder from "../../assets/images/place1-1200.png";
+
+//react-bootstrap card starter
+class Projects extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container>
+          <Row>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={Placeholder} />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={Placeholder} />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            </Row>
+            <Row>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={Placeholder} />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={Placeholder} />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
+    );
+  }
+}
+export default Projects;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from "react";
 import Navigation from "../components/Navigation/Navigation";
 import Banner from "../components/Banner/Banner";
 import About from "../components/About/About";
-import Projects from "../components/Projects/Projocts";
+import Projects from "../components/Projects/Projects";
 import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
 
