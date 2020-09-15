2121538e8844613b46fbc9691b29b51b2f245634
Jody Eggleston <jmeggles@gmail.com>
2020-09-15 13:53:37 -0400

    added starter banner and about info

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,12 +1,24 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react"
+import { Container, Row, Col } from "react-bootstrap"
+import Mypic from "../../assets/images/profile-pic.jpg"
 
-class About extends Component {
-    render () {
-        return (
-            <Fragment>
-                <p>About Component</p>
-            </Fragment>
-        )
-    }
+// stateful since info will be updated often
+const About = () => {
+    return (
+        <Fragment>
+            <Container>
+                <h1>ABOUT ME</h1>
+                <Row>
+                    <Col>
+                        <img src={Mypic} alt="Profile pic of Jody Eggleston" />
+                        <p> Full stack web developer with extensive knowledge in logistics supervision (18 years), experience in nutrition, health & fitness, and criminal justice. I love learning both frontend and backend and have a new passion for coding!
+                        <br/>
+                        Contact me. Lets start a project!</p>
+                    </Col>
+                </Row>
+            </Container>
+        </Fragment>
+    )
 }
+
 export default About;
--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -1,12 +1,16 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
+import { Container } from "react-bootstrap";
 
-class Banner extends Component {
-    render () {
-        return (
-            <Fragment>
-                <p>Banner Component</p>
-            </Fragment>
-        )
-    }
-}
+// stateless since this info will not likely change
+const Banner = () => {
+  return (
+    <Fragment>
+          <Container>
+            <h1>Jody Eggleston</h1>
+            <h4>Full Stack Web Developer</h4>
+          </Container>
+    </Fragment>
+  );
+};
+
 export default Banner;
