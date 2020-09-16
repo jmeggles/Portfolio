4ce9a780c594ab9437d9826b8daabb1d24f368bf
Jody Eggleston <jmeggles@gmail.com>
2020-09-16 11:11:44 -0400

    fixed contact alignment

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,81 +1,80 @@
 import React, { Component, Fragment } from "react";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Row, Col } from "react-bootstrap";
 import "./style.css";
 
 class Contact extends Component {
   render() {
     return (
       <Fragment>
-          <Container className="contactContainer col-lg-5">
-          <h1 className="sectionTitle">CONTACTS</h1>
-        <div>
-          <h5>
-           Central Florida
-          </h5>
-          <h5>
-            <a href={`mailto:jmeggles@gmail.com`}>
-              jmeggles@gmail.com
-            </a>
-          </h5>
-          <h5>
-            <a
-              
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.linkedin.com/in/jody-eggleston"
-            >
-              Linkedin
-            </a>
-          </h5>
-          <h5>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/jmeggles"
-            >
-              GitHub
-            </a>
-          </h5>
-        </div>
-
-        {/* ----------------------------------------------------------- 
+        <Container className="contactContainer">
+        <h1 className="sectionTitle">CONTACTS</h1>
+          <Row>
+            <Col>
+              <div className="contactLinks">
+                <h5>Central Florida</h5>
+                <h5>
+                  <a href={`mailto:jmeggles@gmail.com`}>jmeggles@gmail.com</a>
+                </h5>
+                <h5>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.linkedin.com/in/jody-eggleston"
+                  >
+                    Linkedin
+                  </a>
+                </h5>
+                <h5>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/jmeggles"
+                  >
+                    GitHub
+                  </a>
+                </h5>
+              </div>
+            </Col>
+            {/* ----------------------------------------------------------- 
         
         TEMPORARY: copied in from first portfolio (portfolio-responsive) */}
 
-      
-          <div className="row">
-            <div className="col-md-12">
-              <label>
-                <b>Name</b>
-              </label>
-              <input
-                placeholder="Your Name"
-                // style="margin-bottom: 30px"
-                className="form-control"
-              />
-              <label>
-                <b>Email</b>
-              </label>
-              <input
-                placeholder="example@gmail.com"
-                // style="margin-bottom: 30px"
-                className="form-control"
-              />
-              <label>
-                <b>Message</b>
-              </label>
-              <textarea
-                placeholder="Send me a message!"
-                className="form-control"
-                rows="10"
-              ></textarea>
-              <div>
-                <div>
-                  <Button>Submit</Button>
+            <Col>
+              <div className="contactForm">
+                <div className="col-md-12">
+                  <label>
+                    <b>Name</b>
+                  </label>
+                  <input
+                    placeholder="Your Name"
+                    // style="margin-bottom: 30px"
+                    className="form-control"
+                  />
+                  <label>
+                    <b>Email</b>
+                  </label>
+                  <input
+                    placeholder="example@gmail.com"
+                    // style="margin-bottom: 30px"
+                    className="form-control"
+                  />
+                  <label>
+                    <b>Message</b>
+                  </label>
+                  <textarea
+                    placeholder="Send me a message!"
+                    className="form-control"
+                    rows="10"
+                  ></textarea>
+                  <div>
+                    <div>
+                      <Button>Submit</Button>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
+            </Col>
+          </Row>
         </Container>
       </Fragment>
     );
