00938d8a923fdff1f7231c9293bfe369416347be
Jody Eggleston <jmeggles@gmail.com>
2020-09-16 14:34:50 -0400

    adding serverless connection for contact form

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,13 +1,52 @@
 import React, { Component, Fragment } from "react";
-import { Container, Button, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import axios from "axios";
 import "./style.css";
 
 class Contact extends Component {
+  state = {
+    name: "",
+    message: "",
+    email: "",
+    sent: false,
+    buttonText: "Send Message",
+  };
+  formSubmit = async (e) => {
+    e.preventDefault();
+
+    this.setState({
+      buttonText: "...sending",
+    });
+
+    let data = {
+      name: this.state.name,
+      email: this.state.email,
+      message: this.state.message,
+    };
+
+    axios
+      .post("API_URI", data)
+      .then((res) => {
+        this.setState({ sent: true }, this.resetForm());
+      })
+      .catch(() => {
+        console.log("Message not sent");
+      });
+  };
+
+  resetForm = () => {
+    this.setState({
+      name: "",
+      message: "",
+      email: "",
+    });
+  };
+
   render() {
     return (
       <Fragment>
         <Container className="contactContainer">
-        <h1 className="sectionTitle">CONTACTS</h1>
+          <h1 className="sectionTitle">CONTACT</h1>
           <Row>
             <Col>
               <div className="contactLinks">
@@ -35,44 +74,61 @@ class Contact extends Component {
                 </h5>
               </div>
             </Col>
-            {/* ----------------------------------------------------------- 
-        
-        TEMPORARY: copied in from first portfolio (portfolio-responsive) */}
+            {/* ------------------------- copied in from another project...needs to be formatted-------------------------------------------- */}
+            <Col lg={6} sm={12} className="contactFormCol">
+              <h4>Send me a message!</h4>
+              <form
+                className="contact-form"
+                onSubmit={(e) => this.formSubmit(e)}
+              >
+                <label class="message-name" htmlFor="message-name">
+                  Your Name
+                </label>
+                <br />
+                <input
+                  onChange={(e) => this.setState({ name: e.target.value })}
+                  name="name"
+                  class="message-name"
+                  type="text"
+                  placeholder="Your Name"
+                  value={this.state.name}
+                />
+                <br />
+                <label class="message-email" htmlFor="message-email">
+                  Your Email
+                </label>
+                <br />
+                <input
+                  onChange={(e) => this.setState({ email: e.target.value })}
+                  name="email"
+                  class="message-email"
+                  type="email"
+                  placeholder="your@email.com"
+                  required
+                  value={this.state.email}
+                />
 
-            <Col>
-              <div className="contactForm">
-                <div className="col-md-12">
-                  <label>
-                    <b>Name</b>
-                  </label>
-                  <input
-                    placeholder="Your Name"
-                    // style="margin-bottom: 30px"
-                    className="form-control"
-                  />
-                  <label>
-                    <b>Email</b>
-                  </label>
-                  <input
-                    placeholder="example@gmail.com"
-                    // style="margin-bottom: 30px"
-                    className="form-control"
-                  />
-                  <label>
-                    <b>Message</b>
-                  </label>
-                  <textarea
-                    placeholder="Send me a message!"
-                    className="form-control"
-                    rows="10"
-                  ></textarea>
-                  <div>
-                    <div>
-                      <Button>Submit</Button>
-                    </div>
-                  </div>
+                <br />
+                <label class="message" htmlFor="message-input">
+                  Your Message
+                </label>
+                <br />
+                <textarea
+                  onChange={(e) => this.setState({ message: e.target.value })}
+                  name="message"
+                  class="message-input"
+                  type="text"
+                  placeholder="Please write your message here"
+                  value={this.state.message}
+                  required
+                />
+                <div className="contactButton">
+                  <Button type="submit">
+                    {this.state.ButtonText}
+                    Submit
+                  </Button>
                 </div>
-              </div>
+              </form>
             </Col>
           </Row>
         </Container>
