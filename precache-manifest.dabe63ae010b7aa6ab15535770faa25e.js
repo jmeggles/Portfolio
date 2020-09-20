865d5110d109e2ac4fc1f7167dd0c8de114ca9c8
Jody Eggleston <jmeggles@gmail.com>
2020-09-20 10:44:39 -0400

    Updates

---
+++ precache-manifest.dabe63ae010b7aa6ab15535770faa25e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d720b29ec5e8052d5e2cb673e4659d0",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "73bdbb1f9afb2db87dbd",
+    "url": "/Portfolio/static/css/main.5ff3968e.chunk.css"
+  },
+  {
+    "revision": "a9ddbf851ad80002282f",
+    "url": "/Portfolio/static/js/2.d48005cb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.d48005cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73bdbb1f9afb2db87dbd",
+    "url": "/Portfolio/static/js/main.461fe714.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "e9623a941c853ea4a82d9541d54618b4",
+    "url": "/Portfolio/static/media/banner-laptop.e9623a94.jpg"
+  },
+  {
+    "revision": "649a8f9521f11d2a7fced4bb0300de05",
+    "url": "/Portfolio/static/media/budget-tracker.649a8f95.png"
+  },
+  {
+    "revision": "79fff4238f28905bd337d9e877fb8e35",
+    "url": "/Portfolio/static/media/google-books.79fff423.png"
+  },
+  {
+    "revision": "7171d6b3dc10ea7ccec0f84ce3d8c8d9",
+    "url": "/Portfolio/static/media/house-heroes.7171d6b3.png"
+  },
+  {
+    "revision": "c8dc8c89d7196d534cb404d736586828",
+    "url": "/Portfolio/static/media/profile-pic.c8dc8c89.jpg"
+  },
+  {
+    "revision": "e14a7ecaf5418d4408214fbacdc5ddf7",
+    "url": "/Portfolio/static/media/to-do.e14a7eca.png"
+  }
+]);
