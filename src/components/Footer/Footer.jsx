40117e155f964bc11a71fd965a8737c0730dcd90
Jody Eggleston <jmeggles@gmail.com>
2020-09-27 15:25:02 -0400

    fixed project card spacing when responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,43 @@ const Footer = () => {
     <Fragment>
       <p className="footer">
         Copyright ©2020 |{" "}
-        <span className="footerSpan"><span role="img" aria-label="parkle"> ✨ </span> Jody Eggleston <span role="img" aria-label="parkle"> ✨ </span></span>
+        <span>
+          <span role="img" aria-label="sparkle">
+            {" "}
+            ✨{" "}
+          </span>{" "}
+          <a
+            class="signature"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://jmeggles.github.io/Portfolio"
+          >
+            Jody Eggleston
+          </a>
+        </span>
+        <span>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/jody-eggleston"
+          >
+            <i class="fab fa-linkedin-in footer-icon"></i>
+          </a>
+        </span>
+        <span>
+          {" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/jmeggles"
+          >
+            <i class="fab fa-github footer-icon"></i>
+          </a>
+        </span>
+        <span role="img" aria-label="sparkle">
+          {" "}
+          ✨
+        </span>
       </p>
     </Fragment>
   );
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,8 @@
 import React, { Component, Fragment } from "react";
 import { Card, Button, Col, Container, Row } from "react-bootstrap"; //import all contexts
-import GoogleBooks from "../../assets/images/google-books.png"
-import ToDo from "../../assets/images/to-do.png"
-import Budget from "../../assets/images/budget-tracker.png"
-import Heroes from "../../assets/images/house-heroes.png"
+import GoogleBooks from "../../assets/images/google-books.png";
+import Budget from "../../assets/images/budget-tracker.png";
+import Heroes from "../../assets/images/house-heroes.png";
 import "./style.css";
 
 //react-bootstrap card starter
@@ -12,55 +11,101 @@ class Projects extends Component {
     return (
       <Fragment>
         <Container className="projectContainer">
-        <h1 className="sectionTitle" id="projects">PROJECTS</h1>
+          <h1 className="sectionTitle" id="projects">
+            PROJECTS
+          </h1>
           <Row>
-            <Col md={6} sm={12} >
-              <Card className="projectCard" >
-                <Card.Img className="projectCardImage" variant="top" src={GoogleBooks} />
+            <Col md={4} sm={12}>
+              <Card className="projectCard">
+                <Card.Img
+                  className="projectCardImage"
+                  variant="top"
+                  src={GoogleBooks}
+                />
                 <Card.Body>
                   <Card.Title>Google Books</Card.Title>
                   <Card.Text>
-                  Search, save, view, and buy your favorite books on Google Play store.
+                    Search, save, view, and buy your favorite books on Google
+                    Play store.
                   </Card.Text>
-                  <a target="_blank" rel="noopener noreferrer" href="https://googlebooks-mern.herokuapp.com/"><Button variant="primary">View</Button></a>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/jmeggles/Google_Books"
+                  >
+                    <Button variant="primary" className="m-2">
+                      Code
+                    </Button>
+                  </a>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://googlebooks-mern.herokuapp.com/"
+                  >
+                    <Button variant="primary" className="m-2">
+                      Live
+                    </Button>
+                  </a>
                 </Card.Body>
               </Card>
             </Col>
-            <Col md={6} sm={12} >
-              <Card className="projectCard" >
-                <Card.Img className="projectCardImage" variant="top" src={ToDo} />
-                <Card.Body>
-                  <Card.Title>To-Do App</Card.Title>
-                  <Card.Text>
-                  Simple to-do application to keep daily activities in order.
-                  </Card.Text>
-                  <a target="_blank" rel="noopener noreferrer" href="https://todo-app-with-mern.herokuapp.com/  "><Button variant="primary">View</Button></a>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={6} sm={12} >
-              <Card className="projectCard" >
-                <Card.Img className="projectCardImage" variant="top" src={Budget} />
+            <Col md={4} sm={12}>
+              <Card className="projectCard">
+                <Card.Img
+                  className="projectCardImage"
+                  variant="top"
+                  src={Budget}
+                />
                 <Card.Body>
                   <Card.Title>Budget Tracker</Card.Title>
                   <Card.Text>
-                  Track your budget with offline capabilities. Great when traveling!
+                    Track your budget with offline capabilities. Great when
+                    traveling!
                   </Card.Text>
-                  <a target="_blank" rel="noopener noreferrer" href="https://budget-tracker-for-you.herokuapp.com/ "><Button variant="primary" >View</Button></a>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/jmeggles/Budget_Tracker"
+                  >
+                    <Button className="m-2">Code</Button>
+                  </a>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://budget-tracker-for-you.herokuapp.com/ "
+                  >
+                    <Button className="m-2">Live</Button>
+                  </a>
                 </Card.Body>
               </Card>
             </Col>
-            <Col md={6} sm={12} >
-              <Card className="projectCard" >
-                <Card.Img className="projectCardImage" variant="top" src={Heroes} />
+            <Col md={4} sm={12}>
+              <Card className="projectCard">
+                <Card.Img
+                  className="projectCardImage"
+                  variant="top"
+                  src={Heroes}
+                />
                 <Card.Body>
                   <Card.Title>House Our Heroes</Card.Title>
                   <Card.Text>
-                    Informational site about denied housing due to nature of jobs during the Covid pandemic.
+                    Informational site about denied housing due to nature of
+                    jobs during the pandemic.
                   </Card.Text>
-                  <a target="_blank" rel="noopener noreferrer" href="https://house-our-heroes.herokuapp.com/ "><Button variant="primary" >View</Button></a>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/jmeggles/House-Our-Heroes"
+                  >
+                    <Button className="m-2">Code</Button>
+                  </a>
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://house-our-heroes.herokuapp.com/ "
+                  >
+                    <Button>Live</Button>
+                  </a>
                 </Card.Body>
               </Card>
             </Col>
